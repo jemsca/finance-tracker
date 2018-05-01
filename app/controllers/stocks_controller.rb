65818acb8b828426a8982e9d87bb9fa23b892a4e
jemsca <janet@jems.ca>
2018-05-01 20:12:40 +0000

    Add stock search ajax and errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,16 +3,13 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      if !@stock.nil?
-        render "users/my_portfolio"
-      else
-        flash[:danger] = "No results found"
-        redirect_to my_portfolio_path
+      if @stock.nil?
+        flash.now[:danger] = "No results found"
       end
     else
-      flash[:danger] = "Please enter a search string"
-      redirect_to my_portfolio_path
+      flash.now[:danger] = "Please enter a search string"
     end
+    render partial: "users/result"
   end
 
 end
