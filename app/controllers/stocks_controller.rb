c69046e96c65cab9d8a4e7eaa26e9b6e3fea8f14
jemsca <janet@jems.ca>
2018-05-02 13:31:51 +0000

    Add user and stock association with views

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,9 +3,7 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      if @stock.nil?
-        flash.now[:danger] = "No results found"
-      end
+      flash.now[:danger] = "No results found" unless !@stock.nil?
     else
       flash.now[:danger] = "Please enter a search string"
     end
